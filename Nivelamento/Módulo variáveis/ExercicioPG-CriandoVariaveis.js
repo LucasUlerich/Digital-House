b47a8de8498cfd5e9897ab14c9703345e13295c9
LucasUlerich <lucas.ulerich@outlook.com>
2022-07-01 17:46:20 -0300

    Subindo arquivos(Modulo variaveis)

---
+++ Nivelamento/Módulo variáveis/ExercicioPG-CriandoVariaveis.js
@@ -0,0 +1,6 @@
+// Descrição exercicío:
+
+/* Declare duas variáveis chamadas  idade  e  peso,  e atribua um valor numérico a ambas. */
+
+var idade  = 25
+var peso = 56.3
---
+++ Nivelamento/Módulo variáveis/IntroducaoVariaveis.js
@@ -0,0 +1,16 @@
+// Para criar variaveis primeiro colocamos a palara reservada var, let ou const.
+// Depois o nome da variável, respeitand as regras:
+/* 
+1- Não pode começar com numéros
+2- Não pode conter caracteres especias
+3- Não pode haver espaços
+*/
+
+let pizzaDeQueijo = 10
+var pizzaDeFrango = 20
+
+var nome = "Cesar Michelin"
+
+
+
+var idade = 27
